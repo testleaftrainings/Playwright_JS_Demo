5d73402defa868d0a506117d6b999d4c19f7e3f6
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-11-17 13:54:41 +0530

    Update findDuplicates-sort.js

--- JS demo codes/findDuplicates-sort.js
+++ JS demo codes/findDuplicates-sort.js
@@ -1,13 +1,14 @@
-const arr = [1,3,4,3,2,4,7,6,7]
-
-const sort = arr.sort()
-
-for(let i = 0; i<=sort.length; i++){
-
-   
-        if(sort[i]==sort[i+1]){
-
-            console.log(sort[i])
-        
+// Given array
+const arr = [1, 3, 4, 3, 2, 4, 7, 6, 7];
+ 
+// Sorting the array in ascending order
+const sort = arr.sort();
+ 
+// Loop through the sorted array
+for (let i = 0; i <= sort.length; i++) {
+    // Check if the current element is equal to the next element
+    if (sort[i] == sort[i + 1]) {
+        // If true, there is a duplicate, so log the duplicate element
+        console.log(sort[i]);
     }
 }
